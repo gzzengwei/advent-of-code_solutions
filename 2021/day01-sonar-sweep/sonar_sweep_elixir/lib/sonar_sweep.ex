ba326e8c67128443ea7793ee3a09f6bf9a68ea9b
Wei Zeng <gzzengwei@gmail.com>
2021-12-02 09:30:06 +1100

    2021 day01(sonar sweep) elixir

--- 2021/day01-sonar-sweep/sonar_sweep_elixir/lib/sonar_sweep.ex
+++ 2021/day01-sonar-sweep/sonar_sweep_elixir/lib/sonar_sweep.ex
@@ -3,16 +3,29 @@ defmodule SonarSweep do
   Documentation for `SonarSweep`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SonarSweep.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def measure_from_file(file_path) do
+    File.stream!(file_path)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.to_integer/1)
+    |> measure_increases()
   end
+
+  def measure_increases(list) when is_list(list), do: measure_increases_count(list)
+
+  def measure_increases(%Stream{} = list), do: measure_increases_count(list)
+
+  defp measure_increases_count(list) do
+    list
+    |> Enum.reduce({0, nil}, &aggregate_list/2)
+    |> elem(0)
+  end
+
+  defp aggregate_list(data, {count, last_data}) do
+    with new_count <- calculate_count(count, data > last_data) do
+      {new_count, data}
+    end
+  end
+
+  defp calculate_count(count, true), do: count + 1
+  defp calculate_count(count, _increasesd), do: count
 end
--- 2021/day01-sonar-sweep/sonar_sweep_elixir/test/sonar_sweep_test.exs
+++ 2021/day01-sonar-sweep/sonar_sweep_elixir/test/sonar_sweep_test.exs
@@ -1,8 +1,58 @@
 defmodule SonarSweepTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
   doctest SonarSweep
 
-  test "greets the world" do
-    assert SonarSweep.hello() == :world
+  describe "#measure_increases" do
+    test "given empty list" do
+      assert SonarSweep.measure_increases([]) == 0
+    end
+
+    test "given list of two with increase" do
+      assert SonarSweep.measure_increases([1, 2]) == 1
+    end
+
+    test "given list of three with increase" do
+      assert SonarSweep.measure_increases([1, 2, 3]) == 2
+    end
+
+    test "given list of two with decrease" do
+      assert SonarSweep.measure_increases([2, 1]) == 0
+    end
+
+    test "given list of three with decrease" do
+      assert SonarSweep.measure_increases([3, 2, 1]) == 0
+    end
+
+    test "given list of increase then decrease" do
+      assert SonarSweep.measure_increases([2, 3, 2]) == 1
+    end
+
+    test "given list of decrease then increase" do
+      assert SonarSweep.measure_increases([2, 1, 2]) == 1
+    end
+
+    test "given list of equal" do
+      assert SonarSweep.measure_increases([1, 1]) == 0
+    end
+
+    test "given list of increase and equal" do
+      assert SonarSweep.measure_increases([1, 2, 2]) == 1
+    end
+
+    test "given list of decrease and equal" do
+      assert SonarSweep.measure_increases([2, 1, 1]) == 0
+    end
+
+    test "given list of decrease/decrease/equal" do
+      assert SonarSweep.measure_increases([1, 2, 2, 3, 3, 2, 1]) == 2
+    end
+  end
+
+  describe "#measure_from_file" do
+    test "given input file path" do
+      file_path = System.get_env("INPUT_FILE_PATH", "test/data/input")
+      assert SonarSweep.measure_from_file(file_path) == 1482
+    end
   end
 end
