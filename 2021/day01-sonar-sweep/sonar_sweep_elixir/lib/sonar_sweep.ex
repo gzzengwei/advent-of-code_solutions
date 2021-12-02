348985c0571cdf1043112cbed14601338d032c72
Wei Zeng <gzzengwei@gmail.com>
2021-12-02 17:05:17 +1100

    2021 day01(sonar sweep) elixir - part 2

--- 2021/day01-sonar-sweep/sonar_sweep_elixir/lib/sonar_sweep.ex
+++ 2021/day01-sonar-sweep/sonar_sweep_elixir/lib/sonar_sweep.ex
@@ -10,18 +10,60 @@ defmodule SonarSweep do
     |> measure_increases()
   end
 
-  def measure_increases(list) when is_list(list), do: measure_increases_count(list)
+  def measure_blocks_from_file(file_path) do
+    File.stream!(file_path)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.to_integer/1)
+    |> measure_increases_blocks()
+  end
 
-  def measure_increases(%Stream{} = list), do: measure_increases_count(list)
-
-  defp measure_increases_count(list) do
+  def measure_increases_blocks(list) do
     list
-    |> Enum.reduce({0, nil}, &aggregate_list/2)
+    |> window_blocks()
+    |> measure_increases_count(&compare_block/2)
+  end
+
+  @block_size 3
+  def window_blocks(list) do
+    list
+    |> Enum.with_index()
+    |> Enum.map(&window_block(&1, list))
+    |> Enum.filter(&remove_invalid_block/1)
+  end
+
+  def measure_increases(list) when is_list(list) do
+    measure_increases_count(list, &compare_element/2)
+  end
+
+  def measure_increases(%Stream{} = list) do
+    measure_increases_count(list, &compare_element/2)
+  end
+
+  def window_block({_item, index}, list) do
+    Enum.slice(list, index..(index + @block_size - 1))
+  end
+
+  defp remove_invalid_block(block) when length(block) == @block_size, do: true
+  defp remove_invalid_block(_block), do: false
+
+  defp measure_increases_count(list, compare_func) do
+    list
+    |> Enum.reduce({0, nil}, &aggregate_list(&1, &2, compare_func))
     |> elem(0)
   end
 
-  defp aggregate_list(data, {count, last_data}) do
-    with new_count <- calculate_count(count, data > last_data) do
+  defp compare_block(_current_data, nil), do: false
+
+  defp compare_block(current_data, last_data) do
+    Enum.sum(current_data) > Enum.sum(last_data)
+  end
+
+  defp compare_element(current_data, last_data) do
+    current_data > last_data
+  end
+
+  defp aggregate_list(data, {count, last_data}, compare_func) do
+    with new_count <- calculate_count(count, compare_func.(data, last_data)) do
       {new_count, data}
     end
   end
--- 2021/day01-sonar-sweep/sonar_sweep_elixir/test/sonar_sweep_test.exs
+++ 2021/day01-sonar-sweep/sonar_sweep_elixir/test/sonar_sweep_test.exs
@@ -55,4 +55,54 @@ defmodule SonarSweepTest do
       assert SonarSweep.measure_from_file(file_path) == 1482
     end
   end
+
+  describe "#window_blocks" do
+    test "given list of one" do
+      assert SonarSweep.window_blocks([1]) == []
+    end
+
+    test "given list of two" do
+      assert SonarSweep.window_blocks([1, 2]) == []
+    end
+
+    test "given list of three" do
+      assert SonarSweep.window_blocks([1, 2, 3]) == [[1, 2, 3]]
+    end
+
+    test "given list of multiple of block size" do
+      assert SonarSweep.window_blocks([1, 2, 3, 4, 5, 6]) == [
+               [1, 2, 3],
+               [2, 3, 4],
+               [3, 4, 5],
+               [4, 5, 6]
+             ]
+    end
+
+    test "given list of more than block size" do
+      assert SonarSweep.window_blocks([1, 2, 3, 4, 5, 6, 7]) == [
+               [1, 2, 3],
+               [2, 3, 4],
+               [3, 4, 5],
+               [4, 5, 6],
+               [5, 6, 7]
+             ]
+    end
+  end
+
+  describe "#measure_increases_blocks" do
+    test "given list of one block" do
+      assert SonarSweep.measure_increases_blocks([1, 2, 3]) == 0
+    end
+
+    test "given list of blocks increase in sum" do
+      assert SonarSweep.measure_increases_blocks([1, 2, 3, 4]) == 1
+    end
+  end
+
+  describe "#measure_blocks_from_file" do
+    test "given input file path" do
+      file_path = System.get_env("INPUT_FILE_PATH", "test/data/input")
+      assert SonarSweep.measure_blocks_from_file(file_path) == 1518
+    end
+  end
 end
