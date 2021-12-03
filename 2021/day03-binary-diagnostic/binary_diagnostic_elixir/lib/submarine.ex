793ac07974526766db744407bbb8d60ab28c8ba6
Wei Zeng <gzzengwei@gmail.com>
2021-12-04 07:30:32 +1100

    day03 binary_diagnostic_elixir init

---
+++ 2021/day03-binary-diagnostic/binary_diagnostic_elixir/lib/submarine.ex
@@ -0,0 +1,18 @@
+defmodule Submarine do
+  @moduledoc """
+  Documentation for `Submarine`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Submarine.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
