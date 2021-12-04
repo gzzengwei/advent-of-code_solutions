fd15d7444485b5e99e999fbc46225a63bf1dee02
Wei Zeng <gzzengwei@gmail.com>
2021-12-04 11:40:05 +1100

    2021-day03 Binary Diagnostic part 1

--- 2021/day03-binary-diagnostic/binary_diagnostic_elixir/lib/submarine.ex
+++ 2021/day03-binary-diagnostic/binary_diagnostic_elixir/lib/submarine.ex
@@ -3,16 +3,93 @@ defmodule Submarine do
   Documentation for `Submarine`.
   """
 
-  @doc """
-  Hello world.
+  def power_consumption(inputs) do
+    gamma_rate(inputs) * epsilon_rate(inputs)
+  end
 
-  ## Examples
+  def gamma_rate(inputs) do
+    inputs
+    |> sanitize_input()
+    |> rotae_lists()
+    |> lists_by_count()
+    |> Enum.map(&min_max_list/1)
+    |> binary_list(:gamma)
+    |> convert_to_integer()
+  end
 
-      iex> Submarine.hello()
-      :world
+  def epsilon_rate(inputs) do
+    inputs
+    |> sanitize_input()
+    |> rotae_lists()
+    |> lists_by_count()
+    |> Enum.map(&min_max_list/1)
+    |> binary_list(:epsilon)
+    |> convert_to_integer()
+  end
 
-  """
-  def hello do
-    :world
+  def sanitize_input(inputs) do
+    inputs
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.graphemes/1)
+  end
+
+  def rotae_lists(list) do
+    list
+    |> Enum.map(&Enum.with_index(&1))
+    |> Enum.reduce(%{}, &reduce_rotate_list/2)
+    |> Map.values()
+  end
+
+  def reduce_rotate_list(list, acc) do
+    list
+    |> Enum.reduce(acc, fn {data, index}, acc ->
+      new_data = (acc[index] || []) ++ [data]
+      Map.put(acc, index, new_data)
+    end)
+  end
+
+  def element_occurrences(list) do
+    list
+    |> Enum.reduce(%{}, fn element, acc ->
+      new_count = (acc[element] || 0) + 1
+      Map.put(acc, element, new_count)
+    end)
+  end
+
+  def lists_by_count(lists) do
+    lists
+    |> Enum.map(&element_occurrences/1)
+  end
+
+  def min_max_list(list) do
+    list
+    |> Enum.min_max_by(&elem(&1, 1))
+  end
+
+  def get_max_list(list) do
+    list |> elem(1) |> elem(0)
+  end
+
+  def get_min_list(list) do
+    list |> elem(0) |> elem(0)
+  end
+
+  def binary_list(list, :gamma) do
+    list
+    |> Enum.map(&get_max_list/1)
+    |> Enum.join()
+  end
+
+  def binary_list(list, :epsilon) do
+    list
+    |> Enum.map(&get_min_list/1)
+    |> Enum.join()
+  end
+
+  def convert_to_integer(binary) do
+    binary
+    |> Integer.parse(2)
+    |> elem(0)
   end
 end
--- 2021/day03-binary-diagnostic/binary_diagnostic_elixir/test/submarine_test.exs
+++ 2021/day03-binary-diagnostic/binary_diagnostic_elixir/test/submarine_test.exs
@@ -2,7 +2,151 @@ defmodule SubmarineTest do
   use ExUnit.Case
   doctest Submarine
 
-  test "greets the world" do
-    assert Submarine.hello() == :world
+  describe "#sanitize_input" do
+    test "convert inputs to list of char list" do
+      inputs = """
+      00100
+      11110
+      10110
+      10111
+      10101
+      01111
+      00111
+      11100
+      10000
+      11001
+      00010
+      01010
+      """
+
+      assert Submarine.sanitize_input(inputs) == [
+               ["0", "0", "1", "0", "0"],
+               ["1", "1", "1", "1", "0"],
+               ["1", "0", "1", "1", "0"],
+               ["1", "0", "1", "1", "1"],
+               ["1", "0", "1", "0", "1"],
+               ["0", "1", "1", "1", "1"],
+               ["0", "0", "1", "1", "1"],
+               ["1", "1", "1", "0", "0"],
+               ["1", "0", "0", "0", "0"],
+               ["1", "1", "0", "0", "1"],
+               ["0", "0", "0", "1", "0"],
+               ["0", "1", "0", "1", "0"]
+             ]
+    end
+  end
+
+  describe "#rotae_lists" do
+    test "Given list of multi list" do
+      lists = [
+        ["0", "0", "1", "0", "0"],
+        ["1", "1", "1", "1", "0"],
+        ["1", "0", "1", "1", "0"],
+        ["1", "0", "1", "1", "1"],
+        ["1", "0", "1", "0", "1"],
+        ["0", "1", "1", "1", "1"],
+        ["0", "0", "1", "1", "1"],
+        ["1", "1", "1", "0", "0"],
+        ["1", "0", "0", "0", "0"],
+        ["1", "1", "0", "0", "1"],
+        ["0", "0", "0", "1", "0"],
+        ["0", "1", "0", "1", "0"]
+      ]
+
+      assert Submarine.rotae_lists(lists) == [
+               ["0", "1", "1", "1", "1", "0", "0", "1", "1", "1", "0", "0"],
+               ["0", "1", "0", "0", "0", "1", "0", "1", "0", "1", "0", "1"],
+               ["1", "1", "1", "1", "1", "1", "1", "1", "0", "0", "0", "0"],
+               ["0", "1", "1", "1", "0", "1", "1", "0", "0", "0", "1", "1"],
+               ["0", "0", "0", "1", "1", "1", "1", "0", "0", "1", "0", "0"]
+             ]
+    end
+  end
+
+  describe "#element_occurrences" do
+    test "Given a list" do
+      list = ["0", "0", "0", "1", "1", "1", "1", "0", "0", "1", "0", "0"]
+
+      assert Submarine.element_occurrences(list) == %{"0" => 7, "1" => 5}
+    end
+  end
+
+  describe "#min_max_list" do
+    test "Given a map of pair of count returns tuple in order" do
+      assert Submarine.min_max_list(%{"0" => 5, "1" => 7}) == {{"0", 5}, {"1", 7}}
+      assert Submarine.min_max_list(%{"0" => 9, "1" => 7}) == {{"1", 7}, {"0", 9}}
+    end
+  end
+
+  describe "#gamma_rate" do
+    setup do
+      {:ok, inputs: inputs(), file_inputs: file_inputs()}
+    end
+
+    test "get gamma rate from input of one" do
+      assert Submarine.gamma_rate("00100") == 4
+    end
+
+    test "get gamma rate from multiple inputs by lines", %{inputs: inputs} do
+      assert Submarine.gamma_rate(inputs) == 22
+    end
+
+    test "get gamma rate from file", %{file_inputs: file_inputs} do
+      assert Submarine.gamma_rate(file_inputs) == 941
+    end
+  end
+
+  describe "#epsilon_rate" do
+    setup do
+      {:ok, inputs: inputs(), file_inputs: file_inputs()}
+    end
+
+    test "get epsilon rate from input of one" do
+      assert Submarine.epsilon_rate("10100") == 20
+    end
+
+    test "get epsilon rate from multiple inputs by lines", %{inputs: inputs} do
+      assert Submarine.epsilon_rate(inputs) == 9
+    end
+
+    test "get epsilon rate from file", %{file_inputs: file_inputs} do
+      assert Submarine.epsilon_rate(file_inputs) == 3154
+    end
+  end
+
+  describe "#power_consumption" do
+    setup do
+      {:ok, inputs: inputs(), file_inputs: file_inputs()}
+    end
+
+    test "get epsilon rate from multiple inputs by lines", %{inputs: inputs} do
+      assert Submarine.power_consumption(inputs) == 198
+    end
+
+    test "get epsilon rate from file", %{file_inputs: file_inputs} do
+      assert Submarine.power_consumption(file_inputs) == 2_967_914
+    end
+  end
+
+  def inputs do
+    """
+    00100
+    11110
+    10110
+    10111
+    10101
+    01111
+    00111
+    11100
+    10000
+    11001
+    00010
+    01010
+    """
+  end
+
+  def file_inputs do
+    file_path = System.get_env("INPUT_FILE_PATH", "test/data/input")
+    File.read!(file_path)
   end
 end
